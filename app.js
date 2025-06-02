8aabbdce562f5f1ab473cec496b0dcaf2465c043
Sean Jin <seanjin660@gmail.com>
2025-06-01 18:26:05 -0600

    First Web App Project Finished!

--- app.js
+++ app.js
@@ -23,9 +23,6 @@ app.get("/", async (req, res) => {
         let img1URL = result[0].images.original.url;
         let img2URL = result[1].images.original.url;
         let img3URL = result[2].images.original.url;
-        console.log(img1URL);
-        console.log(img2URL);
-        console.log(img3URL);
         res.render("index.ejs", {
             image1: img1URL,
             image2: img2URL,
@@ -36,13 +33,36 @@ app.get("/", async (req, res) => {
     }
 });
 
-app.post("/search", (req, res) => {
+app.post("/search", async (req, res) => {
     const searchText = req.body.input;
-    console.log(searchText);
     try {
-
+        const response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+            params: {
+                api_key: giphyKey,
+                q: encodeURIComponent(searchText),
+                offset: Math.floor(Math.random() * 499),
+                limit: 6,
+                rating: "g"
+            }
+        });
+        console.log(response);
+        const result = response.data.data;
+        let img1URL = result[0].images.original.url;
+        let img2URL = result[1].images.original.url;
+        let img3URL = result[2].images.original.url;
+        let img4URL = result[3].images.original.url;
+        let img5URL = result[4].images.original.url;
+        let img6URL = result[5].images.original.url;
+        res.render("search.ejs", {
+            image1: img1URL,
+            image2: img2URL,
+            image3: img3URL,
+            image4: img4URL,
+            image5: img5URL,
+            image6: img6URL
+        });
     } catch (error) {
-
+        res.status(400).send(error.message);
     }
 });
 
